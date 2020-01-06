4d914a606ea892a075beaaecb5c30557329abca8
akirayamada-haha <hondamurai99@proton.me>
2020-01-05 18:48:02 -0600

    typos fixed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const apiKey = 'ZwZh41hbFNQDlQubGQG9OdEepfOYOuBaY8EmOV50';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
+const weatherApiKey = 'a1b84e9f01c54a93b59e895e666c594a';
+const weatherSearchURL = 'api.openweathermap.org/data/2.5/forecast';
+
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
   .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -18,7 +21,7 @@ function displayResults(responseJson) {
         <li>
           <h3>${responseJson.data[i].fullName}</h3>
           <p>${responseJson.data[i].description}</p>
-          <a class="park-more-info ${responseJson.data[i].parkCode}" href="#">Click Here for More Info!</a>
+          <a class="park-more-info ${responseJson.data[i].parkCode}" data-latLong=${responseJson.data[i].latLong} href="#">Click Here for More Info!</a>
           <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a>
         </li>
       </ul>`
@@ -27,7 +30,7 @@ function displayResults(responseJson) {
 
 function getParkInfo(searchTerm) {
   const params = {
-    stateCode: searchTerm,
+    q: searchTerm,
     limit: 50,
     start: 1,
     api_key: apiKey,
@@ -89,13 +92,63 @@ function getMoreInfo(clickedCode) {
     })    
 }
 
-function watchInfoLinkClick() {
-    $('#js-results').on("click", ".park-more-info", event => {
-        event.preventDefault();
-        console.log("this is running")
-        const clickedCode = $(event.currentTarget).attr('class').split(' ')[1]
-        getMoreInfo(clickedCode);
+function extractLatLong(clickedLatLong) {
+  return clickedLatLong.split(', ') 
+  .reduce((acc, str) => {
+    const strSplit = str.split(':');
+    acc[strSplit[0]] = strSplit[1];
+    return acc;
+  }, {})
+}
+
+function formatWeatherQueryParams(weatherParams) {
+  const weatherQueryItems = Object.keys(weatherParams)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(weatherParams[key])}`)
+  console.log(weatherQueryItems)
+  return weatherQueryItems.join('&');
+}
+
+function displayWeather(responseJson) {
+  console.log(JSON.stringify(responseJson, null, 4));
+
+  /*for (let i = 0; i < responseJson.data.length; i++){
+    $('#js-results').append(
+      ``
+      )};*/
+}
+
+function generateWeather(clickedLatLong) {
+  const { lat, long } = extractLatLong(clickedLatLong);
+  const weatherParams = {
+    lat,
+    long,
+    appid: weatherApiKey,
+  };
+    const weatherQueryString = formatWeatherQueryParams(weatherParams)
+    const weatherUrl = weatherSearchURL + '?' + weatherQueryString;
+
+    fetch(weatherUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
     })
+    .then(responseJson => displayWeather(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong ${err.message}`)
+    })
+}
+
+function watchInfoLinkClick() {
+  $('#js-results').on("click", ".park-more-info", event => {
+      event.preventDefault();
+      console.log("watchInfoLinkClick is running")
+      const clickedCode = $(event.currentTarget).attr('class').split(' ')[1]
+      const clickedLatLong = $(event.currentTarget).attr('data-latLong')
+      getMoreInfo(clickedCode);
+      generateWeather(clickedLatLong);
+  })
 }
 
 function watchForm() {
